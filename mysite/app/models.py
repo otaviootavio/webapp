ad0ef1de1023e02b543e44e22d4c952fc16e826a
otavio <otavio@usp.br>
2022-10-07 16:38:45 -0300

    Changes at models

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -1,28 +1,35 @@
 import datetime
 from django.db import models
 
-class Livro(models.Model):
-    id = models.IntegerField(primary_key=True)
-    titulo = models.CharField(max_length=200, null=False)
-    isbn = models.CharField(max_length=20, null=False, unique=True)
+class VooBase(models.Model):
+    codigo_voo = models.CharField(max_length=200, null=False)
+    compania_aerea = models.CharField(max_length=200, null=False)
+    dia_da_semana = models.CharField(max_length=200, null=False)
+    horario_partida_base = models.CharField(max_length=200, null=False)
+    duracao_base = models.CharField(max_length=200, null=False)
+    origem = models.CharField(max_length=200, null=False)
+    destino = models.CharField(max_length=200, null=False)
 
     class Meta:
-        db_table = 'livros'
+        db_table = 'voo_base'
 
-class Usuario(models.Model):
-    id = models.IntegerField(primary_key=True)
-    nome = models.CharField(max_length=200, null=False)
+class VooReal(models.Model):
+    ESTADOS_VOO = (
+     ('EMB','Embarcando'),
+     ('CAN','Cancelado'),
+     ('PRG','Programado'),
+     ('TAX','Taxiando'),
+     ('PRT','Pronto'),
+     ('AUT','Autorizado'),
+     ('EMV','Em voo'),
+     ('ATE','Aterrisado'),
+    )
+    
+    voo_base = models.ForeignKey(VooBase, on_delete=models.CASCADE)
+    data_voo = models.DateField()
+    estado_voo = models.CharField(max_length = 1, choices = ESTADOS_VOO)
+    horario_real_chegada = models.TimeField(auto_now=False, auto_now_add=False, null=True)
+    horario_real_partida = models.TimeField(auto_now=False, auto_now_add=False, null=True)
     
     class Meta:
-        db_table = 'usuarios'
-
-class Emprestimo(models.Model):
-    id = models.IntegerField(primary_key=True)
-    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    data_emprestimo = models.DateTimeField(auto_now=True)
-    prazo = models.DurationField(default=datetime.timedelta(days=7))
-    data_devolucao = models.DateTimeField(null=True)
-    
-    class Meta:
-        db_table = 'emprestimos'
+        db_table = 'voo_real'
