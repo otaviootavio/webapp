2bbf263d7fbb48b933027ab3663eecd9226aa2b8
thiagoyc <thiagoyc@gmail.com>
2022-10-14 17:35:35 -0300

    Login screen added

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -9,3 +9,21 @@ def olamundo(request):
 def home(request):
     return render(request,"home.html")
 
+def login(request):
+    if request.method == 'POST':
+        # Process the request if posted data are available
+        username = request.POST['username']
+        password = request.POST['password']
+        # Check username and password combination if correct
+        #user = authenticate(username=username, password=password)
+        if username == 'admin' and password == 'admin':
+            # Save session as cookie to login the user
+            #login(request, user)
+            # Success, now let's login the user.
+            return render(request, "home.html")
+        else:
+            # Incorrect credentials, let's throw an error to the screen.
+            return render(request, "login.html", {'error_message': 'Incorrect username and / or password.'})
+    else:
+        # No post data availabe, let's just show the page to the user.
+        return render(request, "login.html")
